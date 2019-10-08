c5f960f5658a7494074ea523788f81470abd47f4
Jenny Zhang <jenny.zhang@procuraduria.local>
2019-10-08 17:55:53 -0500

    Se hizo el CRUD

--- routes/nota.js
+++ routes/nota.js
@@ -19,6 +19,23 @@ router.post('/nueva-nota', async (req, res) => {
     }
 });
 
+// Get con parametos
+
+router.get('/nota/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const notaDB = await Nota.findOne({
+            _id
+        });
+        res.json(notaDB)
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'Ocurrio un error',
+            error
+        })
+    }
+})
+
 // Get con todos los documentos
 router.get('/nota', async (req, res) => {
     //res.send('utilizando el modelo /nota de models');
@@ -34,5 +51,40 @@ router.get('/nota', async (req, res) => {
     }
 });
 
+//Delete eliminar una nota
+router.delete('/nota/:id', async (req, res) => {
+    const _id = req.params.id;
+    const body = req.body
+    try {
+        const notaDb = await Nota.findByIdAndDelete({
+            _id
+        });
+        res.json(notaDb)
+
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'No encontró el id indicado',
+            error
+        })
+    }
+});
+
+//PUT actualizar una nota
+
+router.put('/nota/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const notaDb = await Nota.findByIdAndUpdate(_id, body, {
+            new: true // new: true es para que postman puede mostrar los datos de actualizacion de una vez
+        });
+        res.json(notaDb)
+
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'La nota ha sido actualizado',
+            error
+        })
+    }
+});
 //Exportacion de router
 module.exports = router;
