06dd33c1b95ee3ef21676b4acea5765abd6f7eb4
Jenny Zhang <jenny.zhang@procuraduria.local>
2019-10-18 11:51:16 -0500

    Prueba de ngrok

--- app.js
+++ app.js
@@ -42,11 +42,11 @@ app.use(
 );
 
 /* reglas de cors */
-/* app.use(function (req, res, next) {
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-}); */
+});
 
 //Rutas
 /* app.get('/', function (req, res) {
